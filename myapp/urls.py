c81f3d34de8b0882ea7afee2b7632ff34b200061
Amitkjha28 <103565661+Amitkjha28@users.noreply.github.com>
2022-07-04 06:18:01 +0530

    view site

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 from Imageupload import settings
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index)
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
